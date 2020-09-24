66e1c47781982c6699a6958e0e08e799af0530ed
Muia23 <wilfred.muema@gmail.com>
2020-09-24 23:48:02 +0300

    Style login form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,28 @@ export class HomeComponent implements OnInit {
   posts:Post[] = [
     { id:1, user_id: 1, community_id: 1, tag_id:1, live_link:'www.google.co.ke', repo_link:'gihub.com',description:'cool application', deadline:'25/September/2020', creation: '23/Sept/2020', is_open_source:true ,is_paid:true, needs_contrib: true }
   ]
+
+  
+  expandpost(complete:boolean) {
+      var big = document.getElementById("fullpost");
+      var small = document.getElementById("shortpost");
+      if (big.style.display === "none") {
+          big.style.display = "block";           
+          small.style.display = "none";           
+      } else {
+          big.style.display = "none";           
+      }
+  }
+  minimisepost(complete:boolean) {
+    var big = document.getElementById("fullpost");
+    var small = document.getElementById("shortpost");
+    if (big.style.display === "block") {
+        big.style.display = "none";           
+        small.style.display = "block";           
+    } else {
+        big.style.display = "block";           
+    }
+}            
   constructor() { }
   
   ngOnInit(): void {
