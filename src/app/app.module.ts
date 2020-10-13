114d37c300e7202caba14ce0ebca4a237e2af32c
Muia23 <wilfred.muema@gmail.com>
2020-10-13 21:00:25 +0300

    ft communities slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { CommunitiesComponent } from './communities/communities.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlickCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
