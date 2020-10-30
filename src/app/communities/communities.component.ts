caf6824f94510f6c7f5935cbb6c48a60b0eb0367
Muia23 <wilfred.muema@gmail.com>
2020-10-30 13:13:47 +0300

    Add a searchbar on communities page and categories filter

--- src/app/communities/communities.component.ts
+++ src/app/communities/communities.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-communities',
   templateUrl: './communities.component.html',
   styleUrls: ['./communities.component.scss']
 })
 export class CommunitiesComponent implements OnInit {
+
 
   constructor() { }
 
